d454cf70c0a3bf856b3f7ff90697f4f75ee9c97b
Devoxin <lukel9914@gmail.com>
2019-12-23 23:28:11 +0000

    Settings

--- src/main/kotlin/life/nekos/bot/commands/Guild.kt
+++ src/main/kotlin/life/nekos/bot/commands/Guild.kt
@@ -3,6 +3,7 @@ package life.nekos.bot.commands
 import life.nekos.bot.framework.annotations.DonorOnly
 import life.nekos.bot.utils.Colors
 import life.nekos.bot.utils.Formats
+import life.nekos.bot.utils.WumpDump
 import me.devoxin.flight.annotations.Command
 import me.devoxin.flight.api.Context
 import me.devoxin.flight.models.Cog
@@ -10,6 +11,7 @@ import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Role
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.TimeUnit
 import kotlin.streams.toList
 
 class Guild : Cog {
@@ -46,6 +48,33 @@ class Guild : Cog {
         CompletableFuture.allOf(*tasks).handle { _, _ ->
             val updated = tasks.filter { !it.isCompletedExceptionally }.size
             msg.editMessage("Done nya! I have set the color of $updated/${tasks.size} roles \\o/").queue()
+        }
+    }
+
+    @Command(aliases = ["r"], description = "Shows server role info", guildOnly = true)
+    fun roles(ctx: Context, role: Role?) {
+        if (role == null) {
+            ctx.send("You must provide the name of the role whose information you want to see, nya~")
+            return
+        }
+
+        val color = role.color
+        val hexStr = color?.let { String.format("%02x%02x%02x", it.red, it.green, it.blue) } ?: "000000"
+        val members = ctx.guild!!.memberCache.getElementsWithRoles(role).joinToString("\n") { it.user.asTag }
+        val permissions = role.permissionsExplicit.joinToString("\n") { it.getName() }
+
+        val membersUrl = WumpDump.paste(members).get(5, TimeUnit.SECONDS)
+        val permissionsUrl = WumpDump.paste(permissions).get(5, TimeUnit.SECONDS)
+
+        ctx.send {
+            setColor(role.color)
+            setTitle("${role.name} (${role.id}) | ${role.asMention}")
+            addField("Color", "#$hexStr", true)
+            addField("Hoisted", role.isHoisted.toString(), true)
+            addField("Managed", role.isManaged.toString(), true)
+            addField("Mentionable", role.isMentionable.toString(), true)
+            addField("Members", "[Click to view]($membersUrl)", true)
+            addField("Permissions", "[Click to view]($permissionsUrl)", true)
         }
     }
 
